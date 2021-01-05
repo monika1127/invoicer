d8f2394e47b2262ff20cff4ae2509dd90d29f573
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-01-05 22:41:30 +0100

    List of invoices added

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <div>Faktury
+            </div>
+        )
+    }
+}
+
+export default NavBar
